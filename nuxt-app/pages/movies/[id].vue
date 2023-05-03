b3ece8b29b249f5095de9917d522ba0e0fb92abc
Gaidash Artem <artemga@hflabs.ru>
2023-05-03 13:53:01 +0300

    dev-Artem: v3.6 Configure useAsyncData with Options

--- nuxt-app/pages/movies/[id].vue
+++ nuxt-app/pages/movies/[id].vue
@@ -3,6 +3,14 @@ const route = useRoute()
 
 const { data } = await useAsyncData(`/movies/${route.params.id}` ,() => {
     return $fetch(`http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`)
+}, {
+    pick: ["Plot", "Title"],
+    // transform(data) {
+    //     return {
+    //       Plot: data.Plot,
+    //       Title: data.Title
+    //     }
+    // }
 })
 </script>
 
