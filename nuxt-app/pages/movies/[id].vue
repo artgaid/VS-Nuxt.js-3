188306405efcb6a63bd0344ae150fbe623ae42e7
Gaidash Artem <artemga@hflabs.ru>
2023-05-03 14:21:37 +0300

    dev-Artem: v3.8 Handle useFetch Errors

--- nuxt-app/pages/movies/[id].vue
+++ nuxt-app/pages/movies/[id].vue
@@ -1,18 +1,26 @@
 <script setup>
 const route = useRoute()
 
-const { data } = await useFetch(`http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`,
+// {"Response":"False","Error":"Incorrect IMDb ID."}
+
+// const status500Url = computed(() => 'https://httpbin.org/status/500')
+
+const movieUrl = computed(() => `http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`)
+
+const { data } = await useFetch(movieUrl.value,
     {
-      pick: ["Plot", "Title"],
-      key: `/movies/${route.params.id}`
+        pick: ["Plot", "Title", "Error"],
+        key: `/movies/${route.params.id}`,
+        // onResponseError() {
+        //     showError({statusCode: 500, statusMessage:'Oh noes!'})
+        // }
     }
 )
 
-// const { data } = await useAsyncData(`/movies/${route.params.id}` ,() => {
-//     return $fetch(`http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`)
-// }, {
-//     pick: ["Plot", "Title"],
-// })
+if (data.value.Error === "Incorrect IMDb ID.") {
+    showError({statusCode: 404, statusMessage:'Page Not Found'})
+}
+
 </script>
 
 <template>
