51dbdf81991766e2f0d92be935fa930b78ca445a
Gaidash Artem <artemga@hflabs.ru>
2023-05-03 14:03:07 +0300

    dev-Artem: v3.7 Call useFetch for a $fetch + useAsyncData Shorthand

--- nuxt-app/pages/movies/[id].vue
+++ nuxt-app/pages/movies/[id].vue
@@ -1,17 +1,18 @@
 <script setup>
 const route = useRoute()
 
-const { data } = await useAsyncData(`/movies/${route.params.id}` ,() => {
-    return $fetch(`http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`)
-}, {
-    pick: ["Plot", "Title"],
-    // transform(data) {
-    //     return {
-    //       Plot: data.Plot,
-    //       Title: data.Title
-    //     }
-    // }
-})
+const { data } = await useFetch(`http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`,
+    {
+      pick: ["Plot", "Title"],
+      key: `/movies/${route.params.id}`
+    }
+)
+
+// const { data } = await useAsyncData(`/movies/${route.params.id}` ,() => {
+//     return $fetch(`http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`)
+// }, {
+//     pick: ["Plot", "Title"],
+// })
 </script>
 
 <template>
