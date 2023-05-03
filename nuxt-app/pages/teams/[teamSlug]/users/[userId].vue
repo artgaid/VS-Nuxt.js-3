d4c0e532668811324f63ce98cb8ea8bbcdfbb769
Gaidash Artem <artemga@hflabs.ru>
2023-05-03 12:08:33 +0300

    dev-Artem: v2.3 Dynamic Route Params in Nuxt.js

---
+++ nuxt-app/pages/teams/[teamSlug]/users/[userId].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import { useRoute } from "vue-router";
+
+  const route = useRoute()
+  const message = computed(() => `I am on team ${route.params.teamSlug } and my user id is ${ route.params.userId }`)
+</script>
+
+<template>
+  <p>
+    {{ message }}
+  </p>
+</template>
