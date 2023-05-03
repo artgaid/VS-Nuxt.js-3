2067d5ec8987068bb94e98b4db38e6799051092f
Gaidash Artem <artemga@hflabs.ru>
2023-05-03 15:12:22 +0300

    dev-Artem: v4.1 Call the useHead Composable for Good SEO

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -7,3 +7,11 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup>
+useHead({
+    titleTemplate: (titleChunk) => {
+        return titleChunk ? `${titleChunk} | My movie site` : 'My movie site'
+    }
+})
+</script>
--- nuxt-app/pages/movies/[id].vue
+++ nuxt-app/pages/movies/[id].vue
@@ -9,7 +9,7 @@ const movieUrl = computed(() => `http://www.omdbapi.com/?apikey=8e3f600b&i=${rou
 
 const { data } = await useFetch(movieUrl.value,
     {
-        pick: ["Plot", "Title", "Error"],
+        pick: ["Plot", "Title", "Poster" ,"Error"],
         key: `/movies/${route.params.id}`,
         // onResponseError() {
         //     showError({statusCode: 500, statusMessage:'Oh noes!'})
@@ -20,6 +20,16 @@ const { data } = await useFetch(movieUrl.value,
 if (data.value.Error === "Incorrect IMDb ID.") {
     showError({statusCode: 404, statusMessage:'Page Not Found'})
 }
+
+useHead({
+    title: data.value.Title,
+    meta: [
+        { name: "description", content: data.value.Plot },
+        { property: "og:description", content: data.value.Plot },
+        { property: "og:image", content: data.value.Poster },
+        { name: "twitter:card", content: `summary_large_image` },
+    ],
+})
 
 </script>
 
