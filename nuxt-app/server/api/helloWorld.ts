902dbdc8dfc2f42bca5c0f27c27e1d8ee800eab8
Gaidash Artem <artemga@hflabs.ru>
2023-05-03 16:08:48 +0300

    dev-Artem: v4.6 Handle Different Nuxt API Request Methods

--- nuxt-app/server/api/helloWorld.ts
+++
@@ -1,9 +0,0 @@
-export default defineEventHandler((event) => {
-  // if --- http://localhost:3000/api/helloWorld?hello=world
-  return {
-    message: getQuery(event)
-  }
-  // return --- "message": {
-  //     "hello": "world"
-  //   }
-})
